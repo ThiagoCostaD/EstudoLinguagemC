892e20b2e1be6c8067445e8999ee806f7b757986
Thiago Costa <thiagoocdiniz@gmail.com>
2022-08-21 00:49:10 -0300

    Iniciando Linguagem C

---
+++ teste01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Curso de programação C");
+    return 0;
+}
---
+++ teste02.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Mais um teste");
+    return 0;
+}
